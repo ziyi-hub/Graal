568ea74803ae22c3caebf1ce9f12dd243819a610
Ziyi WANG <ziyiwang1027@gmail.com>
2022-09-12 12:24:11 +0200

    ajout navigation

--- sample/src/App.vue
+++ sample/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <ul>
-      <li><router-link to="/"></router-link></li>
-      <li><router-link to="/mon_espace">MON ESPACE</router-link></li>
-      <li><router-link to="/mes_produits">MES PRODUITS</router-link></li>
-      <li><router-link to="/lister_un_produit">LISTER UN PRODUIT</router-link></li>
-      <li><router-link to="/faq">FAQ</router-link></li>
-    </ul>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container px-4">
+        <a class="navbar-brand">GS GRAAL LIST by Graal Spotter</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+            <li class="nav-item"><router-link to="/"></router-link></li>
+            <li class="nav-item"><router-link to="/mon_espace">MON ESPACE</router-link></li>
+            <li class="nav-item"><router-link to="/mes_produits">MES PRODUITS</router-link></li>
+            <li class="nav-item"><router-link to="/lister_un_produit">LISTER UN PRODUIT</router-link></li>
+            <li class="nav-item"><router-link to="/faq">FAQ</router-link></li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
 
     <router-view></router-view>
-
 
   </div>
 </template>
@@ -34,7 +44,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
@@ -52,14 +61,11 @@ li {
 }
 
 a {
-  color: #42b983;
+  color: black;
+  text-decoration: none;
 }
 
-.footer_box {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  flex-flow: column wrap;
+.navbar-collapse{
+  flex-grow: unset;
 }
 </style>
