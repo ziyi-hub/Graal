4cd24969995265a2e6d5f0b865a6d80355833c33
Ziyi WANG <ziyiwang1027@gmail.com>
2022-09-13 16:49:34 +0200

    ajout fonctionnalité clickMasquer et clickMasquerConfirm

--- sample/src/components/signup.vue
+++ sample/src/components/signup.vue
@@ -51,13 +51,29 @@
             <!-- Mot de passe -->
             <div class="col-md-6">
               <label for="password" class="form-label">Mot de passe</label>
-              <input type="password" class="form-control" id="password">
+              <div class="input-group g-3">
+                <input type="password" class="form-control" id="password">
+                <span class="input-group-text input-group-text-1" v-on:click="clickMasquer()">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                  </svg>
+                </span>
+              </div>
             </div>
 
             <!-- Confirmer le mot de passe -->
             <div class="col-md-6">
               <label for="passwordConfirm" class="form-label">Confirmer le mot de passe</label>
-              <input type="password" class="form-control" id="passwordConfirm">
+              <div class="input-group g-3">
+                <input type="password" class="form-control" id="passwordConfirm">
+                <span class="input-group-text input-group-text-2" v-on:click="clickMasquerConfirm()">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                  </svg>
+                </span>
+              </div>
             </div>
 
             <!-- Checkbox m'abonner a la newsletter -->
@@ -89,6 +105,23 @@ import Navigation from "./navigation.vue";
     components: { Navigation },
 
     methods : {
+      clickMasquer(){
+        let input = document.getElementById("password");
+        if (input.type === "password") {
+          input.type = "text";
+        } else {
+          input.type = "password";
+        }
+      },
+
+      clickMasquerConfirm(){
+        let input = document.getElementById("passwordConfirm");
+        if (input.type === "password") {
+          input.type = "text";
+        } else {
+          input.type = "password";
+        }
+      },
     },
 
     computed : {
@@ -97,5 +130,7 @@ import Navigation from "./navigation.vue";
 </script>
 
 <style>
-
+.input-group-addon{
+  border: 1px solid black;
+}
 </style>
