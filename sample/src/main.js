48a2f21f7f807e2bba50600c5fedeaa84a8104db
Ziyi WANG <ziyiwang1027@gmail.com>
2022-09-16 11:28:21 +0200

    optimiser constructeur

--- sample/src/main.js
+++ sample/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-
+import router from './router'
 import store from './store'
 
 import axios from 'axios'
@@ -11,40 +10,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-import Homepage from './views/home.vue'
-import NotFound from './views/notfound.vue'
-import Signup from './views/signup.vue'
-import Login from './views/login.vue'
-import Description from './views/description.vue'
-import Espace from './views/espace.vue'
-import ListerUnProduit from './views/lister_un_produit.vue'
-import ModesPaiement from './views/modes_de_paiement.vue'
-import Faq from './views/faq.vue'
-import SuiviProduits from './views/suivi_de_produits.vue'
-import Adresses from './views/adresses.vue'
-import ForgetPassword from './views/forgotPassword.vue'
-
 Vue.use(VueAxios, axios)
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  mode : "history",
-  routes : [
-    {path: "/", component: Homepage},
-    {path: "/inscription", component: Signup},
-    {path: "/connexion", component: Login},
-    {path: "/description", component: Description},
-    {path: "/espace", component: Espace},
-    {path: "/modes_paiement", component: ModesPaiement},
-    {path: "/lister_un_produit", component: ListerUnProduit},
-    {path: "/faq", component: Faq},
-    {path: "/adresses", component: Adresses},
-    {path: "/suivi_produits", component: SuiviProduits},
-    {path: "/forgot_password", component: ForgetPassword},
-    {path: "*", component: NotFound} //A mettre a la fin de tous les routeurs
-  ]
-})
-
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
